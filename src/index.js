de03b38188bcceca3ff0bca250ddea875abd9034
Simeon Tverdal <Simeon.Tverdal@sintef.no>
2020-10-19 11:54:17 +0200

    Revert

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-
-import App from './js/App.js';
-import Store from './js/store/Store.js';
-
-ReactDOM.render(<Provider store={Store} ><App /></Provider>, document.getElementById('app'));
--- src/js/components/atoms/PageHeading/PageHeading.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import './pageheading.css';
-
-const PageHeading = ({ title }) =>
-    <h1 className="page-heading">{title}</h1>;
-
-export default PageHeading;
